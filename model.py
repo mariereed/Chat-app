d44cefa534e042f333ffe9488d77b1d735722bda
marie <fshbon677@gmail.com>
2017-12-30 20:42:12 +0000

    Added model.py

---
+++ model.py
@@ -0,0 +1,27 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+#-----------------------------------------
+
+
+def connect_to_db(app, db_uri):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PostgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
+    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
+    from flask import Flask
+    app = Flask(__name__)
+
+    connect_to_db(app, 'postgresql:///chatappdb')
